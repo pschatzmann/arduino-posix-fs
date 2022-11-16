7f684e967ceb620bd61af0e8495d9bc59382595f
Phil Schatzmann <phil.schatzmann@gmail.com>
2022-11-16 17:06:32 +0100

    FileSystems.cpp

--- src/FileSystems.cpp
+++ src/FileSystems.cpp
@@ -10,4 +10,10 @@ FSLoggerClass FSLogger;
 #endif
 Registry DefaultRegistry;
 
+// Shared vector for all open files
+Vector<RegEntry *> Registry::open_files;
+// Shared vector for all file systems
+Vector<FileSystemBase *> Registry::file_systems;
+
+
 }
